8844cbe827c07038ba11f5ea6b304ec9f897042b
wulei(GW00297735) <gw00297735@tech.gwm.cn>
2022-07-25 13:30:35 +0800

    perf:优化eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
   /* 指定如何解析语法。*/
   parser: 'vue-eslint-parser',
   // recommended 用来启用推荐的规则
+  // 将 prettier 加到 extends 数组的最后，出现冲突时会优先prettier的规则，就不会出现兼容性问题
   extends: [
     'eslint:recommended',
     'plugin:vue/vue3-recommended',
@@ -15,52 +16,64 @@ module.exports = {
     'plugin:prettier/recommended',
     'prettier',
   ],
+  // 配置解析器 https://eslint.bootcss.com/docs/user-guide/configuring
+  // https://eslint.vuejs.org/user-guide/#usage
   parserOptions: {
-    ecmaVersion: 12,
-    parser: '@typescript-eslint/parser',
+    ecmaVersion: 12, // 指定想要使用的 ECMAScript 版本
+    parser: '@typescript-eslint/parser', // 指定解析器
     sourceType: 'module',
     ecmaFeatures: {
-      jsx: true,
+      jsx: true, // 启用 JSX
     },
   },
-  // eslint-plugin-vue @typescript-eslint/eslint-plugin eslint-plugin-prettier的缩写
   plugins: ['vue', '@typescript-eslint', 'prettier'],
   rules: {
+    // 取消类型检查提示
     '@typescript-eslint/ban-ts-ignore': 'off',
+    // 禁止未使用过的变量
     '@typescript-eslint/no-unused-vars': 'off',
+    // 在函数和类方法上需要显式的返回类型
     '@typescript-eslint/explicit-function-return-type': 'off',
+    // 禁止使用any类型
     '@typescript-eslint/no-explicit-any': 'off',
+    // 除导入语句外，禁止使用require语句
     '@typescript-eslint/no-var-requires': 'off',
+    // 禁止使用空函数
     '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
+    // typescript中的interface以及type不存在变量提升的问题，因此针对它们特别的加了两行规则，保证eslint见到interface或者type在声明前使用时不会报错
+    '@typescript-eslint/no-use-before-define': ['error', { ignoreTypeReferences: true }],
+    // 禁止使用@ts-注解
+    '@typescript-eslint/ban-ts-comment': ['warn', {
+      'ts-expect-error': 'allow-with-description',
+      'ts-ignore': 'allow-with-description',
+      'ts-nocheck': 'allow-with-description',
+      'ts-check':  'allow-with-description'
+    }],
+    // 禁用特定类型
     '@typescript-eslint/ban-types': 'off',
+    // 禁止使用!后缀运算符进行非null断言
     '@typescript-eslint/no-non-null-assertion': 'off',
+    // 在导出的函数和类的公共类方法上需要显式的返回值和参数类型
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    // 导入其他文件时，是否要求写后缀
+    'import/extensions': 'off',
+    // 禁用var
     'no-var': 'error',
     'prettier/prettier': 'error',
     // 禁止出现console
-    // 'no-console': 'warn',
-    // 禁用debugger
-    // 'no-debugger': 'warn',
-    // 禁止出现重复的 case 标签
-    'no-duplicate-case': 'warn',
+    'no-console': 'warn',
     // 禁止出现空语句块
     'no-empty': 'warn',
     // 禁止不必要的括号
     'no-extra-parens': 'off',
-    // 禁止对 function 声明重新赋值
-    'no-func-assign': 'warn',
-    // 禁止在 return、throw、continue 和 break 语句之后出现不可达代码
-    'no-unreachable': 'warn',
     // 强制所有控制语句使用一致的括号风格
-    curly: 'warn',
+    'curly': 'warn',
     // 要求 switch 语句中有 default 分支
     'default-case': 'warn',
     // 强制尽可能地使用点号
     'dot-notation': 'warn',
     // 要求使用 === 和 !==
-    eqeqeq: 'warn',
+    'eqeqeq': 'warn',
     // 禁止 if 语句中 return 语句之后有 else 块
     'no-else-return': 'warn',
     // 禁止出现空函数
@@ -69,41 +82,33 @@ module.exports = {
     'no-lone-blocks': 'warn',
     // 禁止使用多个空格
     'no-multi-spaces': 'warn',
-    // 禁止多次声明同一变量
-    'no-redeclare': 'warn',
     // 禁止在 return 语句中使用赋值语句
     'no-return-assign': 'warn',
     // 禁用不必要的 return await
     'no-return-await': 'warn',
-    // 禁止自我赋值
-    'no-self-assign': 'warn',
     // 禁止自身比较
     'no-self-compare': 'warn',
-    // 禁止不必要的 catch 子句
-    'no-useless-catch': 'warn',
     // 禁止多余的 return 语句
     'no-useless-return': 'warn',
-    // 禁止变量声明与外层作用域的变量同名
+    // 禁止变量声明覆盖外层作用域的变量
     'no-shadow': 'off',
-    // 允许delete变量
-    'no-delete-var': 'off',
     // 强制数组方括号中使用一致的空格
     'array-bracket-spacing': 'warn',
     // 强制在代码块中使用一致的大括号风格
     'brace-style': 'warn',
-    // 强制使用骆驼拼写法命名约定
-    camelcase: 'warn',
+    // 强制使用驼峰拼写法命名约定
+    'camelcase': 'warn',
     // 强制使用一致的缩进
-    indent: 'off',
+    'indent': 'off',
     // 强制在 JSX 属性中一致地使用双引号或单引号
-    // 'jsx-quotes': 'warn',
+    'jsx-quotes': 'warn',
     // 强制可嵌套的块的最大深度4
-    // 'max-depth': 'warn',
-    // 强制最大行数 300
-    // "max-lines": ["warn", { "max": 1200 }],
-    // 强制函数最大代码行数 50
-    // 'max-lines-per-function': ['warn', { max: 70 }],
-    // 强制函数块最多允许的的语句数量20
+    'max-depth':  ["warn", { "max": 4 }],
+    // 强制文件最大行数，不建议写超大文件
+    "max-lines": ["warn", { "max": 1200 }],
+    // 强制函数最大代码行数
+    'max-lines-per-function': ['warn', { max: 100 }],
+    // 强制函数块最多允许的的语句数量
     'max-statements': ['warn', 100],
     // 强制回调函数最大嵌套深度
     'max-nested-callbacks': ['warn', 3],
@@ -111,7 +116,7 @@ module.exports = {
     'max-params': ['warn', 5],
     // 强制每一行中所允许的最大语句数量
     'max-statements-per-line': ['warn', { max: 1 }],
-    // 要求方法链中每个调用都有一个换行符
+    // 要求方法链中每个调用都有一个换行符。ignoreChainWithDepth允许在同一行成链的深度
     'newline-per-chained-call': ['warn', { ignoreChainWithDepth: 3 }],
     // 禁止 if 作为唯一的语句出现在 else 语句中
     'no-lonely-if': 'warn',
@@ -120,11 +125,11 @@ module.exports = {
     // 禁止出现多行空行
     'no-multiple-empty-lines': 'warn',
     // 禁止出现;
-    semi: ['warn', 'never'],
+    'semi': ['warn', 'never'],
     // 强制在块之前使用一致的空格
     'space-before-blocks': 'warn',
     // 强制在 function的左括号之前使用一致的空格
-    // 'space-before-function-paren': ['warn', 'never'],
+    'space-before-function-paren': ['warn', 'never'],
     // 强制在圆括号内使用一致的空格
     'space-in-parens': 'warn',
     // 要求操作符周围有空格
@@ -132,21 +137,15 @@ module.exports = {
     // 强制在一元操作符前后使用一致的空格
     'space-unary-ops': 'warn',
     // 强制在注释中 // 或 /* 使用一致的空格
-    // "spaced-comment": "warn",
+    "spaced-comment": "warn",
     // 强制在 switch 的冒号左右有空格
     'switch-colon-spacing': 'warn',
     // 强制箭头函数的箭头前后使用一致的空格
     'arrow-spacing': 'warn',
-    'no-var': 'warn',
+    // 建议使用const
     'prefer-const': 'warn',
+    // 建议使用剩余参数代替 arguments 
     'prefer-rest-params': 'warn',
-    'no-useless-escape': 'warn',
-    'no-irregular-whitespace': 'warn',
-    'no-prototype-builtins': 'warn',
-    'no-fallthrough': 'warn',
-    'no-extra-boolean-cast': 'warn',
-    'no-case-declarations': 'warn',
-    'no-async-promise-executor': 'warn',
   },
   globals: {
     defineProps: 'readonly',
